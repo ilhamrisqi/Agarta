3cb3af4d4f0622bdf9f83546b583920fb569a1a3
ilhamrisqi <92148866+ilhamrisqi@users.noreply.github.com>
2022-10-28 23:53:37 +0700

    halaman home,contact,about

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -1,26 +1,14 @@
 @extends('layout')
 @section('container')
 
-
-    <div class="row">
-        <div class="col-12 bg-light px-4 py-4">
-            <div class="row d-flex justify-content-center text-center">
-                <div class="col-3 ">
-                    <p><a href="" class="link-dark fw-bold" style="text-decoration: none">Why Us?</a></p>
-                </div>
-                <div class="col-3">
-                    <p><a href="" class="link-dark fw-bold" style="text-decoration: none">Services</a></p>
-                </div>
-                <div class="col-3 ">
-                    <p><a href="" class="link-dark fw-bold" style="text-decoration: none"  >Reach us</a></p>
-                </div>
-
-            </div>
-            <div class="row">
-                <div class="col-12 text-center mt-5">
-                    <p><a href="" class="link-dark fw-bold" style="text-decoration: none">&#169 2022 Agarta Photography. All rights reserved</a></p>
-                </div>
-            </div>
+    <div class="justify-content-md-center">
+        <div class="row row-cols-lg-3">
+            <div class="">Column</div>
+            <div class=" col-lg-4">Column</div>
+            <div class="col-lg-4">Column</div>
+            <div class="col-lg-4">Column</div>
+            <div class=" col-lg-4">Column</div>
+            <div class=" col-lg-4">Column</div>
         </div>
     </div>
 @endsection
