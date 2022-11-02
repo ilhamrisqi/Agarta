db69f36580ea47d65c8df3fa7d0406387513c5e1
ilhamrisqi <92148866+ilhamrisqi@users.noreply.github.com>
2022-11-02 21:51:16 +0700

    penambahan fitur

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -10,7 +10,10 @@
             @foreach($tampilan as $tes)
 
                     <div class="col-lg-4  col-md-6 col-sm-12 p-lg-3 p-md-3">
-                        <div class="card mx-auto " style="width: 15rem;">
+                        <div class="card mx-auto  "data-aos="fade-up"
+                             data-aos-anchor-placement="center-center"
+                             data-aos-duration="5000"
+                             style="width: 15rem;">
 
                             <img src="/image/{{$tes['image']}}" class="card-img-top w-100 " >
                             <a class="text-center p-3 fw-light btn "style="font-family:'Helvetica Neue'" href="/subgallery/{{$tes['id']}}">{{$tes['subtext']}}</a>
--- resources/views/subgallery.blade.php
+++ resources/views/subgallery.blade.php
@@ -7,7 +7,9 @@
         <div class="row justify-content-center p-5">
             <h1 class="text-center fw-light fs-1 p-3" style="font-family: 'Helvetica Neue'">{{$title}}</h1>
             @foreach($tampilan ['image1'] as $gallery)
-            <div class="col-12 text-center ">
+            <div class="col-12 text-center " data-aos="fade-up"
+                 data-aos-duration="5000"
+                 data-aos-easing="ease-in-sine">
                 <img class="p-3" src="/image potrait/{{$gallery}}" alt="" style="height: 550px; width: 400px;" >
             </div>
             @endforeach
